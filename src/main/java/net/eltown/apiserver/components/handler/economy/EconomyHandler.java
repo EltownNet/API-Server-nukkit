1db08ff6270dd6ed255c44dfefb66f6154619d53
Proxma <39440218+Proxma3838@users.noreply.github.com>
2021-04-20 20:04:21 +0200

    Switched to TinyRabbit

--- src/main/java/net/eltown/apiserver/components/handler/economy/EconomyHandler.java
+++ src/main/java/net/eltown/apiserver/components/handler/economy/EconomyHandler.java
@@ -3,73 +3,55 @@ package net.eltown.apiserver.components.handler.economy;
 import com.rabbitmq.client.*;
 import lombok.SneakyThrows;
 import net.eltown.apiserver.Server;
+import net.eltown.apiserver.components.tinyrabbit.TinyRabbitListener;
 
 import java.nio.charset.StandardCharsets;
 
 public class EconomyHandler {
 
-    private final Channel channel;
     private final Server server;
     private final EconomyProvider provider;
+    private final TinyRabbitListener listener;
 
     @SneakyThrows
     public EconomyHandler(final Server server, final Connection connection) {
         this.server = server;
         this.provider = new EconomyProvider(server);
-        this.channel = connection.createChannel();
-        this.channel.queueDeclare("economy", false, false, false, null);
-        this.channel.queuePurge("economy");
-        this.channel.basicQos(1);
+        this.listener = new TinyRabbitListener("localhost");
+        //this.channel = connection.createChannel();
+        //this.channel.queueDeclare("economy", false, false, false, null);
+        //this.channel.queuePurge("economy");
+        //this.channel.basicQos(1);
 
         this.startCallbacking();
     }
 
     public void startCallbacking() {
         server.getExecutor().execute(() -> {
-            try {
-                final DeliverCallback callback = (tag, delivery) -> {
-                    final AMQP.BasicProperties replyProps = new AMQP.BasicProperties
-                            .Builder()
-                            .correlationId(delivery.getProperties().getCorrelationId())
-                            .build();
 
-                    final String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
-
-                    final String[] request = message.split("//");
-
-                    this.server.log("[" + Thread.currentThread().getName() + "] " + "[«] " + request[0].toUpperCase());
-
-                    switch (EconomyCalls.valueOf(request[0].toUpperCase())) {
-                        case REQUEST_GETMONEY:
-                            this.publish(delivery, replyProps, EconomyCalls.CALLBACK_MONEY.name() + "//" + this.provider.get(request[1]));
-                            break;
-                        case REQUEST_SETMONEY:
-                            this.provider.set(request[1], Double.parseDouble(request[2]));
-                            this.publish(delivery, replyProps, EconomyCalls.CALLBACK_NULL.name());
-                            break;
-                        case REQUEST_ACCOUNTEXISTS:
-                            this.publish(delivery, replyProps, EconomyCalls.CALLBACK_ACCOUNTEXISTS.name() + "//" + this.provider.has(request[1]));
-                            break;
-                        case REQUEST_CREATEACCOUNT:
-                            this.provider.create(request[1], Double.parseDouble(request[2]));
-                            this.publish(delivery, replyProps, EconomyCalls.CALLBACK_NULL.name());
-                            break;
-                    }
-                };
-
-                this.channel.basicConsume("economy", false, callback, (consumerTag -> {
-                }));
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
+            this.listener.callback((request -> {
+                switch (EconomyCalls.valueOf(request.getKey().toUpperCase())) {
+                    case REQUEST_GETMONEY:
+                        request.answer(EconomyCalls.CALLBACK_MONEY.name(), String.valueOf(this.provider.get(request.getData()[1])));
+                        //this.publish(delivery, replyProps, EconomyCalls.CALLBACK_MONEY.name() + "//" + this.provider.get(request[1]));
+                        break;
+                    case REQUEST_SETMONEY:
+                        this.provider.set(request.getData()[1], Double.parseDouble(request.getData()[2]));
+                        request.answer(EconomyCalls.CALLBACK_NULL.name());
+                        //this.publish(delivery, replyProps, EconomyCalls.CALLBACK_NULL.name());
+                        break;
+                    case REQUEST_ACCOUNTEXISTS:
+                        request.answer(EconomyCalls.CALLBACK_ACCOUNTEXISTS.name(), String.valueOf(this.provider.has(request.getData()[1])));
+                        //this.publish(delivery, replyProps, EconomyCalls.CALLBACK_ACCOUNTEXISTS.name() + "//" + this.provider.has(request[1]));
+                        break;
+                    case REQUEST_CREATEACCOUNT:
+                        this.provider.create(request.getData()[1], Double.parseDouble(request.getData()[2]));
+                        request.answer(EconomyCalls.CALLBACK_NULL.name());
+                        //this.publish(delivery, replyProps, EconomyCalls.CALLBACK_NULL.name());
+                        break;
+                }
+            }), "API Server", "economy");
         });
-    }
-
-    @SneakyThrows
-    private void publish(final Delivery delivery, final AMQP.BasicProperties props, String message) {
-        this.channel.basicPublish("", delivery.getProperties().getReplyTo(), props, message.getBytes(StandardCharsets.UTF_8));
-        this.channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
-        this.server.log("[" + Thread.currentThread().getName() + "] " + "[»] " + message.split("//")[0]);
     }
 
 }
--- src/main/java/net/eltown/apiserver/components/handler/player/PlayerHandler.java
+++ src/main/java/net/eltown/apiserver/components/handler/player/PlayerHandler.java
@@ -3,16 +3,12 @@ package net.eltown.apiserver.components.handler.player;
 import com.rabbitmq.client.*;
 import lombok.SneakyThrows;
 import net.eltown.apiserver.Server;
-import net.eltown.apiserver.components.handler.economy.EconomyCalls;
-import net.eltown.apiserver.components.handler.economy.EconomyProvider;
 import net.eltown.apiserver.components.handler.player.data.SyncPlayer;
-
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+import net.eltown.apiserver.components.tinyrabbit.TinyRabbitListener;
 
 public class PlayerHandler {
 
-    private final Channel callbackChannel, receivingChannel;
+    private final TinyRabbitListener listener;
     private final Server server;
     private final PlayerProvider provider;
 
@@ -20,13 +16,8 @@ public class PlayerHandler {
     public PlayerHandler(final Server server, final Connection connection) {
         this.server = server;
         this.provider = new PlayerProvider(server);
-        this.callbackChannel = connection.createChannel();
-        this.callbackChannel.queueDeclare("playersync", false, false, false, null);
-        this.callbackChannel.queuePurge("playersync");
-        this.callbackChannel.basicQos(1);
-
-        this.receivingChannel = connection.createChannel();
-        this.receivingChannel.queueDeclare("playersyncReceive", false, false, false, null);
+        this.listener = new TinyRabbitListener("localhost");
+        this.listener.throwExceptions(true);
 
         this.startCallbacking();
         this.startReceiving();
@@ -34,83 +25,50 @@ public class PlayerHandler {
 
     public void startReceiving() {
         this.server.getExecutor().execute(() -> {
-            try {
-                DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-                    String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+            this.listener.receive((delivery -> {
+                final String[] request = delivery.getData();
 
-                    final String[] request = message.split("//");
+                System.out.println("got: " + delivery.getKey());
 
-                    this.server.log("[" + Thread.currentThread().getName() + "] " + "[«] " + request[0].toUpperCase());
+                switch (PlayerCalls.valueOf(delivery.getKey())) {
+                    case REQUEST_SETSYNC:
+                        SyncPlayer set = new SyncPlayer(
+                                request[2],
+                                request[3],
+                                request[4],
+                                request[5],
+                                request[6],
+                                request[7],
+                                true
+                        );
+                        this.provider.set(request[1], set);
+                        break;
+                    case REQUEST_SETNOSYNC:
+                        SyncPlayer player = this.provider.get(request[1]);
+                        player.setCanSync(false);
+                        this.provider.set(request[1], player);
+                        break;
+                }
 
-                    switch (PlayerCalls.valueOf(request[0].toUpperCase())) {
-                        case REQUEST_SETSYNC:
-                            SyncPlayer set = new SyncPlayer(
-                                    request[2],
-                                    request[3],
-                                    request[4],
-                                    request[5],
-                                    request[6],
-                                    request[7],
-                                    true
-                            );
-                            this.provider.set(request[1], set);
-                            break;
-                        case REQUEST_SETNOSYNC:
-                            SyncPlayer player = this.provider.get(request[1]);
-                            player.setCanSync(false);
-                            this.provider.set(request[1], player);
-                            break;
-                    }
-                };
-
-                this.receivingChannel.basicConsume("playersyncReceive", true, deliverCallback, consumerTag -> {
-                });
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
+            }), "API Server", "playersyncReceive");
         });
     }
 
     public void startCallbacking() {
         this.server.getExecutor().execute(() -> {
-            try {
-                final DeliverCallback callback = (tag, delivery) -> {
-                    final AMQP.BasicProperties replyProps = new AMQP.BasicProperties
-                            .Builder()
-                            .correlationId(delivery.getProperties().getCorrelationId())
-                            .build();
-
-                    final String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
-
-                    final String[] request = message.split("//");
-
-                    this.server.log("[" + Thread.currentThread().getName() + "] " + "[«] " + request[0].toUpperCase());
-
-                    switch (PlayerCalls.valueOf(request[0].toUpperCase())) {
-                        case REQUEST_SYNC:
-                            final SyncPlayer syncPlayer = this.provider.get(request[1]);
-                            if (syncPlayer.isCanSync()) {
-                                this.publish(delivery, replyProps, PlayerCalls.GOT_SYNC.name() + "//" + syncPlayer.getInvString() + "//" + syncPlayer.getEcString() + "//" + syncPlayer.getHealth() + "//" + syncPlayer.getFood() + "//" + syncPlayer.getExp() + "//" + syncPlayer.getLevel());
-                            } else {
-                                this.publish(delivery, replyProps, PlayerCalls.GOT_NOSYNC.name() + "//" + request[1]);
-                            }
-                            break;
-                    }
-                };
-
-                this.callbackChannel.basicConsume("playersync", false, callback, (consumerTag -> {
-                }));
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
+            this.listener.callback((request -> {
+                switch (PlayerCalls.valueOf(request.getKey())) {
+                    case REQUEST_SYNC:
+                        final SyncPlayer syncPlayer = this.provider.get(request.getData()[1]);
+                        if (syncPlayer.isCanSync()) {
+                            request.answer(PlayerCalls.GOT_SYNC.name(), syncPlayer.getInvString() + "//" + syncPlayer.getEcString() + "//" + syncPlayer.getHealth() + "//" + syncPlayer.getFood() + "//" + syncPlayer.getExp() + "//" + syncPlayer.getLevel());
+                        } else {
+                            request.answer(PlayerCalls.GOT_NOSYNC.name(), request.getData()[1]);
+                        }
+                        break;
+                }
+            }), "API Server", "playersync");
         });
-    }
-
-    @SneakyThrows
-    private void publish(final Delivery delivery, final AMQP.BasicProperties props, String message) {
-        this.callbackChannel.basicPublish("", delivery.getProperties().getReplyTo(), props, message.getBytes(StandardCharsets.UTF_8));
-        this.callbackChannel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
-        this.server.log("[" + Thread.currentThread().getName() + "] " + "[»] " + message.split("//")[0]);
     }
 
 }
